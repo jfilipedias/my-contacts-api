aa49dbeec41d833a351e9b91c4339e47a90bf6c8
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-09-11 18:12:28 -0300

    Refactor the create function from contacts repository

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,5 +1,7 @@
 const { v4 } = require('uuid')
 
+const db = require('../../database')
+
 let contacts = [
   {
     id: v4(),
@@ -36,19 +38,15 @@ class ContactsRepository {
     })
   }
 
-  create({ name, email, phone, category_id }) {
-    return new Promise((resolve) => {
-      const newContact = {
-        id: v4(),
-        name,
-        email,
-        phone,
-        category_id,
-      }
+  async create({ name, email, phone, category_id }) {
+    const [row] = await db.query(
+      `INSERT INTO contacts(name, email, phone, category_id) 
+      VALUES($1, $2, $3, $4)
+      RETURNING *`,
+      [name, email, phone, category_id]
+    )
 
-      contacts.push(newContact)
-      resolve(newContact)
-    })
+    return row
   }
 
   update(id, { name, email, phone, category_id }) {
