8fc35ffcfd372afcf324be531d3754ef7d9a49d2
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-09-13 10:07:57 -0300

    Adds error handling to the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+
+require('express-async-errors')
+
 const router = require('./app/routes')
 
 const app = express()
 
 app.use(express.json())
+
 app.use(router)
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  console.log(error)
+  response.sendStatus(500)
+})
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'))
