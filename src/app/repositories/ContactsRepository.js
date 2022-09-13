fee0b5446a9c579450abec9c8cb5d4452845729d
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-09-13 08:48:21 -0300

    Refactor the update cotacts

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -52,24 +52,17 @@ class ContactsRepository {
     return row
   }
 
-  update(id, { name, email, phone, category_id }) {
-    return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      }
-
-      contacts = contacts.map((contact) => {
-        if (contact.id === id) return updatedContact
-
-        return contact
-      })
-
-      resolve(updatedContact)
-    })
+  async update(id, { name, email, phone, category_id }) {
+    const [row] = await db.query(
+      `
+      UPDATE contacts 
+      SET name = $1, email = $2, phone = $3, category_id = $4
+      WHERE id = $5
+      RETURNING *
+      `,
+      [name, email, phone, category_id, id]
+    )
+    return row
   }
 
   deleteById(id) {
